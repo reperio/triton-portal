09c648dfb073bb74f038eed2b38a2e9865b54aad
Spencer Dangel <dangelspencer@gmail.com>
2018-02-07 14:30:10 -0500

    Converted index and knexfile to use new Config class

--- config.js
+++
@@ -1,9 +0,0 @@
-const env = process.env.NODE_ENV || 'development';
-
-const config_name = `config-${env}`;
-
-console.log(`loading ${config_name}`);
-
-const config = require(`./${config_name}`);
-
-module.exports = config;
--- config/test.js
+++ config/test.js
@@ -1,9 +1,8 @@
 module.exports = {
-    jsonSecret: '496d7e4d-eb86-4706-843b-5ede72fad0e8',
     db: {
         user: 'reperio',
         password: 'reperio',
         host: 'localhost',
-        database: 'reperio_triton_portal_dev'
+        database: 'reperio_triton_portal_test'
     }
 };
